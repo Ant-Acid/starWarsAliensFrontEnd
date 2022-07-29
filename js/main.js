8825668b8be747ecb74a31bde65444d875d344d4
Catalin <csiegling@gmail.com>
2022-07-28 22:29:16 -0500

    added comments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-document.querySelector('#getButton').addEventListener('click', apiRequest)
+document.querySelector('#getButton').addEventListener('click', apiRequest) //add event listener to the button
 
-async function apiRequest() {
-    const alienName = document.querySelector('input').value
-    try {
-        const res = await fetch(`https://star-trek-aliens-api-backend.herokuapp.com/api/${alienName}`)
-        const data = await res.json()
-        console.log(data);
+async function apiRequest() { //async function to make the request
+    const alienName = document.querySelector('input').value //get the value of the input
+    try { //try to make the request
+        const res = await fetch(`https://star-trek-aliens-api-backend.herokuapp.com/api/${alienName}`) //make the request
+        const data = await res.json() //get the data
+        console.log(data); //log the data
 
-        document.getElementById('alienName').innerText = data.speciesName
+        document.getElementById('alienName').innerText = data.speciesName //set the text of the h1
         document.getElementById('alienWorld').innerText = data.homeworld
         document.getElementById('alienFeatures').innerText = data.features
         document.getElementById('alienFacts').innerText = data.interestingFact
--- lcars.js
+++ lcars.js
@@ -1,19 +1,19 @@
-document.addEventListener("touchstart", function() {},false);
+document.addEventListener("touchstart", function() {},false); //fixes mobile touch scrolling
 
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('.scroll-top a').fadeIn();
-    } else {
-        $('.scroll-top a').fadeOut();
+$(window).scroll(function() { //scroll to top button
+    var height = $(window).scrollTop(); //get the height of the window
+    if (height > 100) { //if the height is greater than 100px
+        $('.scroll-top a').fadeIn(); //show the scroll to top button
+    } else { //otherwise
+        $('.scroll-top a').fadeOut(); //hide the scroll to top button
     }
 });
 
-$(document).ready(function() {
-    $("#scroll-top").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
+$(document).ready(function() { //scroll to top button
+    $("#scroll-top").click(function(event) { //when the button is clicked
+        event.preventDefault(); //prevent the default action
+        $("html, body").animate({ scrollTop: 0 }, "slow"); //scroll to the top of the page
+        return false; //prevent the default action
     });
 
 });
